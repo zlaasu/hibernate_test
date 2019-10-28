60e80cd0b34c37130afa9f7f7327b32bb345d25e
Janek <zlaasu@gmail.com>
2019-10-28 11:46:56 +0100

    [day 1, part 1, task 2]

---
+++ src/main/java/pl/coderslab/book/Book.java
@@ -0,0 +1,19 @@
+package pl.coderslab.book;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+}
