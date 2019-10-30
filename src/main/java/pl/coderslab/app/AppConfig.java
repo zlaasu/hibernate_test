ec0b6686a20dcfae3cc8bc24521b642b4d5e244f
Janek <zlaasu@gmail.com>
2019-10-30 09:55:03 +0100

    [day 3, part 1, task 1]

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -7,13 +7,18 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import pl.coderslab.author.AuthorConverter;
 
 import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -46,9 +51,21 @@ public class AppConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(getBookAuthorConverter());
     }
+
     @Bean
     public AuthorConverter getBookAuthorConverter() {
         return new AuthorConverter();
     }
 
+    @Bean(name = "localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+        return localeResolver;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
