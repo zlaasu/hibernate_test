cda0beac5e6e6fa9e55ef9d5ddb08e490daed5fc
Janek <zlaasu@gmail.com>
2019-10-30 10:48:38 +0100

    [day 3, part 1, task 5]

--- src/main/java/pl/coderslab/publisher/Publisher.java
+++ src/main/java/pl/coderslab/publisher/Publisher.java
@@ -2,9 +2,12 @@ package pl.coderslab.publisher;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
 import pl.coderslab.book.Book;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -17,8 +20,15 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String name;
 
     @OneToMany(mappedBy = "publisher")
     private List<Book> books;
+
+    @NIP
+    public String nip;
+
+    @REGON
+    public String regon;
 }
